c9e93a7de6a5052f258310de44ea66303425557f
netxbax <rootn66@gmail.com>
2018-12-29 02:05:08 -0600

    make migration and auth
    make migration and auth of the proyect

---
+++ database/migrations/2018_12_29_080229_create_movements_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMovementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('type',50);
+            $table->dateTime('movement_date');
+            $table->unsignedInteger('category_id');
+            $table->string('descripcion',1000);
+            $table->string('image')->nullable();
+            $table->unsignedInteger('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movements');
+    }
+}
